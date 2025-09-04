84fb7c528048bd2c81efe065a6c0c5456d8a08a5
Júlio Magalhães <julio.magalhaes@maps.com.br>
2025-09-04 00:13:07 -0300

    fix: small fixes

--- frontend/src/app/transactions/edit/[id]/page.tsx
+++ frontend/src/app/transactions/edit/[id]/page.tsx
@@ -1,10 +1,12 @@
-import type { EditTransactionModalProps } from "@/interfaces/componentProps";
+"use client";
+
 import { TransactionType } from "@/app/models/transaction";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select } from "@/components/ui/select";
-import { useState, useEffect } from "react";
+import type { EditTransactionModalProps } from "@/interfaces/componentProps";
 import { formatToBRL } from "@/utils/format";
+import { useEffect, useState } from "react";
 
 export function EditTransactionModal({
   isOpen,
