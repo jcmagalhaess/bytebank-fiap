9f13cf9c84d548d9aaf354bb5c2ae1849e0722e2
Júlio Magalhães <julio.magalhaes@maps.com.br>
2025-10-11 18:50:17 -0300

    fix: small fixes

--- frontend/angular-remote/src/app/app.routes.ts
+++ frontend/angular-remote/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/user-login/user-login').then((c) => c.UserLogin),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/user-registration/user-registration').then((c) => c.UserRegistration),
+  },
+];
