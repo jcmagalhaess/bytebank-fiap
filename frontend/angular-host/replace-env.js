9647be5096d35b11c882136e4cb468f01d0ef1d5
Júlio Magalhães <julio.magalhaes@maps.com.br>
2025-10-14 15:10:06 -0300

    fix: small fixes

--- frontend/angular-host/replace-env.js
+++ frontend/angular-host/replace-env.js
@@ -6,8 +6,8 @@ const manifestContent = fs.readFileSync(manifestPath, 'utf8');
 
 // Substitui a variável de ambiente pelo valor real
 const replacedContent = manifestContent.replace(
-  'process.env.ANGULAR_REMOTE',
-  process.env.ANGULAR_REMOTE || 'http://localhost:4201' // Fallback para desenvolvimento
+  'ANGULAR_REMOTE_URL',
+  process.env.ANGULAR_REMOTE_URL || 'http://localhost:4201' // Fallback para desenvolvimento
 );
 
 fs.writeFileSync(manifestPath, replacedContent, 'utf8');
